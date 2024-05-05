648fa27bd6945ce2c791fab8aa1d6d700bc5d618
JamaluddinNasrul <nasruljamal785@gmail.com>
2024-05-05 20:15:31 +0700

    belajar

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Account from './screen/Account';
+import Mebelku from './screen/Mebelku';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Beranda from './screen/Beranda';
+
+const Tab = createBottomTabNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name='Mebelku' component={Mebelku} options={{
+          tabBarLabel: 'Mebelku',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+      
+      }} />
+      <Tab.Screen name='Account' component={Account} options={{
+          tabBarLabel: 'Account',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="account" color={color} size={26} />
+          ),
+      
+      }} />
+
+     
+
+    </Tab.Navigator>
+   </NavigationContainer>
+  );
+};
+  
+
+export default App
+
+const styles = StyleSheet.create({});
---
+++ src/screen/Mebelku.jsx
@@ -0,0 +1,34 @@
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native'
+import React from 'react'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const Data =[
+  {id: 1, namaIcon: 'truck', judulIcon:'pengiriman'},
+  {id: 2, namaIcon: 'dollar-sign', judulIcon:'pembayaran'},
+  {ID: 3, namaIcon: 'shopping-bag', judulIcon:'belanja'},
+
+];
+
+const Mebelku = () => {
+  return (
+    <View style={{padding:20}}>
+      <FlatList
+      data={Data}
+      horizontal={true}
+      renderItem={({item}) => (
+      <TouchableOpacity style={{width:50, marginRight:35}}>
+        <View style={{width:50, height:50, backgroundColor:'gray', borderRadius:10, alignItems:'center', justifyContent:'center'}}>
+          <MaterialCommunityIcons name={item.namaIcon} color='white' size={30} />
+        </View>
+        <Text style={{color:'black', textAlign:'center'}}>{item.judulIcon}</Text>
+      </TouchableOpacity>
+
+  )} />
+    </View>
+  )
+}
+
+
+export default Mebelku
+
+const styles = StyleSheet.create({})
